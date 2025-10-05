f7399a78f61546254e03940a37b4ce3f8485c77c
hyochan <106391445+rlagycks@users.noreply.github.com>
2025-10-05 22:59:44 +0900

    chore : ci 디버딩용 로직 추가 - MoauApplicationTests.java에 ci 디버깅용 로직 추가

--- src/test/java/com/moau/moau/MoauApplicationTests.java
+++ src/test/java/com/moau/moau/MoauApplicationTests.java
@@ -2,9 +2,24 @@ package com.moau.moau;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 
 @SpringBootTest
 class MoauApplicationTests {
+
+    @DynamicPropertySource
+    static void mysqlProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", () ->
+                String.format("jdbc:mysql://%s:%s/%s?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC&characterEncoding=utf8mb4",
+                        System.getenv("CI_DB_HOST"),
+                        System.getenv("CI_DB_PORT"),
+                        System.getenv("CI_DB_NAME")));
+        registry.add("spring.datasource.username", () -> System.getenv("CI_DB_USER"));
+        registry.add("spring.datasource.password", () -> System.getenv("CI_DB_PASSWORD"));
+
+        registry.add("logging.level.org.springframework", () -> "DEBUG");
+    }
 
     @Test
     void contextLoads() {
