4953e610344b89ef0be1bbc0d95fc98f7f993adf
hyochan <106391445+rlagycks@users.noreply.github.com>
2025-10-05 23:08:18 +0900

    chore : ci 디버딩용 로직 추가 - MoauApplicationTests.java에 ci 디버깅용 로직 추가

--- src/test/java/com/moau/moau/MoauApplicationTests.java
+++ src/test/java/com/moau/moau/MoauApplicationTests.java
@@ -10,15 +10,25 @@ class MoauApplicationTests {
 
     @DynamicPropertySource
     static void mysqlProperties(DynamicPropertyRegistry registry) {
+        String dbHost = System.getenv("CI_DB_HOST");
+        String dbPort = System.getenv("CI_DB_PORT");
+        String dbName = System.getenv("CI_DB_NAME");
+        String dbUser = System.getenv("CI_DB_USER");
+        String dbPass = System.getenv("CI_DB_PASSWORD");
+
+        System.out.println("--- Reading CI Environment Variables ---");
+        System.out.println("--- CI_DB_HOST: " + dbHost);
+        System.out.println("--- CI_DB_PORT: " + dbPort);
+        System.out.println("--- CI_DB_NAME: " + dbName);
+        System.out.println("--- CI_DB_USER: " + dbUser);
+        System.out.println("--- CI_DB_PASSWORD length: " + (dbPass != null ? dbPass.length() : "null"));
+        System.out.println("--------------------------------------");
+
         registry.add("spring.datasource.url", () ->
                 String.format("jdbc:mysql://%s:%s/%s?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC&characterEncoding=utf8mb4",
-                        System.getenv("CI_DB_HOST"),
-                        System.getenv("CI_DB_PORT"),
-                        System.getenv("CI_DB_NAME")));
-        registry.add("spring.datasource.username", () -> System.getenv("CI_DB_USER"));
-        registry.add("spring.datasource.password", () -> System.getenv("CI_DB_PASSWORD"));
-
-        registry.add("logging.level.org.springframework", () -> "DEBUG");
+                        dbHost, dbPort, dbName));
+        registry.add("spring.datasource.username", () -> dbUser);
+        registry.add("spring.datasource.password", () -> dbPass);
     }
 
     @Test
