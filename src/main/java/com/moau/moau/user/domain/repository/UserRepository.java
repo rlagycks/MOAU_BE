0ae910b296725c1e8943362b45fdbb70b824e46c
sunchips0103 <rnrxodid@naver.com>
2025-10-14 01:31:43 +0900

    feat(user): UserRepository 구현 (이메일 기반 사용자 조회 추가)

---
+++ src/main/java/com/moau/moau/user/domain/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.moau.moau.user.domain.repository;
+import com.moau.moau.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
