ae6d0e1e625c03a1dbdeb3018dc3073599e4a400
rim_yejun_ <jayjun.rim@gmail.com>
2025-11-15 18:22:35 +0900

    @ConfigurationPropertiesScan 추가

--- src/main/java/com/moau/moau/MoauApplication.java
+++ src/main/java/com/moau/moau/MoauApplication.java
@@ -2,8 +2,11 @@ package com.moau.moau;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+
+@ConfigurationPropertiesScan
 @EnableJpaAuditing
 @SpringBootApplication
 public class MoauApplication {
