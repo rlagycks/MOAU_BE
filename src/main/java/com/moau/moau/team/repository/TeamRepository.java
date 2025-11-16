ba27be347d0724b119e36685dbc9a8059b41f464
sunchips0103 <rnrxodid@naver.com>
2025-11-16 22:51:26 +0900

    feat(team): TeamRepository에 팀 조회 및 초대코드 중복 검사 메서드 추가

--- src/main/java/com/moau/moau/team/repository/TeamRepository.java
+++ src/main/java/com/moau/moau/team/repository/TeamRepository.java
@@ -2,8 +2,12 @@ package com.moau.moau.team.repository;
 
 import com.moau.moau.team.domain.Team;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+import java.util.List;
+
 public interface TeamRepository extends JpaRepository<Team, Long> {
+
+    List<Team> findByOwnerId(Long ownerId);
+
+    boolean existsByInviteCode(String inviteCode);
 }
