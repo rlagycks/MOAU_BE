42056bb4b9280a6cae4336f7bf2aade32ac68bdd
sunchips0103 <rnrxodid@naver.com>
2025-11-16 22:45:53 +0900

    refactor(auth): KakaoLoginResponse 패키지 위치 정리

--- src/main/java/com/moau/moau/auth/dto/response/KakaoLoginResponse.java
+++ src/main/java/com/moau/moau/auth/dto/response/KakaoLoginResponse.java
@@ -1,4 +1,4 @@
 // src/main/java/com/moau/moau/user/dto/response/auth/KakaoLoginResponse.java
-package com.moau.moau.user.dto.response.auth;
+package com.moau.moau.auth.dto.response;
 
 public record KakaoLoginResponse(String accessToken, String refreshToken, boolean isNewUser) {}
