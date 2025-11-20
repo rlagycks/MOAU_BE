e5159a277fb17866ac438dab1bbf941d78625605
hyochan <106391445+rlagycks@users.noreply.github.com>
2025-11-20 14:11:40 +0900

    fix(teamMember): roles 자료형 고정 - main 서버에 hibernate가 role을 tinyint로 착각하는 문제 수정

--- src/main/java/com/moau/moau/team/domain/TeamMember.java
+++ src/main/java/com/moau/moau/team/domain/TeamMember.java
@@ -31,7 +31,7 @@ public class TeamMember {
     private User user;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition = "varchar(255)")
     private TeamMemberRole role;
 
     @Enumerated(EnumType.STRING)
